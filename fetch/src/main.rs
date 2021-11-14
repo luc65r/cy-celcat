4154b593e4baa3a779302c73c5ad0b611cac771d
Lucas Ransan <lucas@ransan.tk>
2021-11-14 15:39:26 +0100

    [fetch] add event subcommand

--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -1,6 +1,9 @@
 use celcat::{
-    fetchable::calendar::{CalView, CalendarData, CalendarDataRequest},
-    Celcat, Group, GroupId,
+    fetchable::{
+        calendar::{CalView, CalendarData, CalendarDataRequest},
+        event::{Event, EventRequest},
+    },
+    Celcat, Student, StudentId, CourseId,
 };
 use chrono::NaiveDateTime;
 use clap::Parser;
@@ -25,10 +28,15 @@ enum SubCommand {
         #[clap(short, long)]
         id: String,
     },
+    Event {
+        #[clap(short, long)]
+        id: String,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let opts: Opts = Opts::parse();
 
     let mut celcat = Celcat::new("https://services-web.u-cergy.fr/calendar").await?;
@@ -36,14 +44,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match opts.subcmd {
         SubCommand::Calendar { start, end, id } => {
-            let res: CalendarData<Group> = celcat
+            let res: CalendarData<Student> = celcat
                 .fetch(CalendarDataRequest {
                     start,
                     end,
-                    res_type: Group,
+                    res_type: Student,
                     cal_view: CalView::Month,
-                    federation_ids: GroupId(id),
+                    federation_ids: StudentId(id),
                     colour_scheme: 3,
+                })
+                .await?;
+            println!("{:#?}", res);
+        }
+        SubCommand::Event { id } => {
+            let res: Event = celcat
+                .fetch(EventRequest {
+                    event_id: CourseId(id),
                 })
                 .await?;
             println!("{:#?}", res);
